2714884e704041246c8b90f7e67a3acb856fd015
ASWIN <rbai8913@gmail.com>
2025-04-05 15:29:29 +0530

    filename

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import GenderCheckBox from "./GenderCheckBox";
+import GenderCheckBox from "./GenderCheckBox.jsx";
 import { Link } from "react-router-dom";
 import useSignup from "../../hooks/useSignup.js";
 
